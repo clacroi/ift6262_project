d0b91415ea9eeb4aaa848a54e7d033e7b414ed5a
Corentin <corent1.lacroix@gmail.com>
2017-03-12 21:26:38 -0400

    new NN with batch norm and more parameters

--- models_v0.py
+++ models_v0.py
@@ -7,6 +7,7 @@ import keras.models as models
 from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Merge, Permute
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D, Deconvolution2D
 from keras.layers.normalization import BatchNormalization
+from keras.layers.wrappers import TimeDistributed
 
 import theano.tensor as T
 from keras import backend as K
@@ -105,6 +106,46 @@ def generate_samples_v02(samples_per_epoch, batch_size, path, fn_list, meanStd_d
 
             batch_X = np.array(batch_images)
             batch_Y = batch_X[:,:,16:48,16:48].copy()
-            batch_X[:,:,16:48,16:48] = 0
+            batch_X[:,:,16:48,16:48] = 0.0
 
             yield (batch_X, batch_Y)
+
+def model_v03():
+
+    # Define model
+    autoencoder = models.Sequential()
+    # Encoder
+    autoencoder.add(Layer(input_shape=(3, 64, 64)))
+    autoencoder.add(
+        Convolution2D(64, 5, 5, activation='relu', border_mode='same', input_shape=(3, 64, 64), dim_ordering='th'))
+    autoencoder.add(BatchNormalization(mode=0, axis=1))
+    autoencoder.add(MaxPooling2D((2, 2), border_mode='same', dim_ordering='th'))
+    autoencoder.add(
+        Convolution2D(32, 4, 4, activation='relu', border_mode='same', input_shape=(64, 32, 32), dim_ordering='th'))
+    autoencoder.add(BatchNormalization(mode=0, axis=1))
+    autoencoder.add(MaxPooling2D((2, 2), border_mode='same', dim_ordering='th'))
+    autoencoder.add(
+        Convolution2D(16, 3, 3, activation='relu', border_mode='same', input_shape=(32, 16, 16), dim_ordering='th'))
+    autoencoder.add(BatchNormalization(mode=0, axis=1))
+    autoencoder.add(MaxPooling2D((2, 2), border_mode='same', dim_ordering='th'))
+    # Output : (16, 8, 8)
+
+    # Intermediate layer
+    autoencoder.add(Reshape((16, 64)))
+    autoencoder.add(TimeDistributed(Dense(64), input_shape=(16, 64)))
+    autoencoder.add(Reshape((16, 8, 8)))
+
+    # Decoder
+    autoencoder.add(
+        Convolution2D(16, 3, 3, activation='relu', border_mode='same', input_shape=(16, 8, 8), dim_ordering='th'))
+    autoencoder.add(UpSampling2D((2, 2), dim_ordering='th'))
+    autoencoder.add(
+        Convolution2D(32, 4, 4, activation='relu', border_mode='same', input_shape=(32, 16, 16), dim_ordering='th'))
+    autoencoder.add(UpSampling2D((2, 2), dim_ordering='th'))
+    autoencoder.add(
+        Convolution2D(3, 5, 5, activation='sigmoid', border_mode='same', input_shape=(3, 32, 32), dim_ordering='th'))
+    # Output : (3, 32, 32)
+
+    autoencoder.compile(optimizer='adadelta', loss='mse')
+
+    return autoencoder
--- train.py
+++ train.py
@@ -9,11 +9,11 @@ from keras import backend as K
 from models_v0 import *
 from preproc import *
 
-PROJ_PATH = '/home/ubuntu/project'
-BATCH_SIZE = 200
-NB_EPOCH = 100
-NB_SAMPLES_PER_EPOCH = 82610
-NB_VAL_SAMPLES = 40438
+PROJ_PATH = '/home/corentin/Documents/Polytechnique/Deep Learning/Projet'
+BATCH_SIZE = 2
+NB_EPOCH = 10
+NB_SAMPLES_PER_EPOCH = 10
+NB_VAL_SAMPLES = 10
 FIT_STYLE = "gen"
 
 
@@ -91,9 +91,9 @@ y_val = x_val[:, :, 16:48, 16:48].copy() # construct y_val
 x_val[:, :, 16:48, 16:48] = 0 # fill x_val central region with 0s
 
 # Convolutional Auto-Encoder v0.1
-model_name = "convautoencoder_v02"
+model_name = "convautoencoder_v03"
 print("Compiling model...")
-autoencoder = model_v02()
+autoencoder = model_v03()
 autoencoder.summary()
 
 print("Fitting model...")
@@ -105,5 +105,5 @@ autoencoder_train = evaluate_model(autoencoder, "gen", BATCH_SIZE, NB_EPOCH, NB_
 
 autoencoder.save_weights('./Results/' + model_name + '.h5')
 print(autoencoder_train.history)
-with open('./Results/' + model_name + '_trainHistory.pkl', 'wb') as output:
+with open('./Results/Models_v0' + model_name + '_trainHistory.pkl', 'wb') as output:
     pickle.dump(autoencoder_train.history, output, pickle.HIGHEST_PROTOCOL)
