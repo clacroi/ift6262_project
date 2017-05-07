c4c8ae46f40e9a5a0ddf6b5d30d01747575326c1
Rudy Roberto <rudy_portugal@MacBook-Pro-de-Rudy-Roberto.local>
2017-05-07 15:21:34 -0400

    Modified train params

--- models_vae.py
+++ models_vae.py
@@ -127,7 +127,7 @@ def model_vae_10(batch_size, original_dim):
         # D_KL(Q(z|X,y) || P(z|X)); calculate in closed form as both dist. are Gaussian
         kl = 0.5 * K.sum(K.exp(enc_s) + K.square(enc_m) - 1. - enc_s, axis=-1)
 
-        return K.mean(recon + kl)
+        return K.mean(recon) + kl
 
     model = Model(inputs=im, outputs=decoder_outputs)
     model.compile(optimizer='adam', loss=vae_loss)
